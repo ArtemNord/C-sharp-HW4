1516543e08d119b1c4d3b1fccda17f77293869c2
Nord.A <mr.nordakov@gmail.com>
2022-06-17 16:56:04 +0300

    add C# HW1

---
+++ C# HW1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу с циклом, которая принимает на вход два числа (А и В)
+// и возводит число А в натуральную степень В.
+
+void Exponentiation(int number0, int number1)
+{
+    int result = number0;
+    for (int i = 1; i < number1; i++)
+    {
+        result = result * number0;
+    }
+    
+    //double result = Math.Pow(number0, number1); - Если не цикл, то можно решить так.
+
+    Console.WriteLine($"Ответ: {result}");
+}
+
+Console.Write ("Введите число А: ");
+int number0 = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите число B: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Exponentiation(number0, number1);
---
+++ C# HW2/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+void ArraySum(int number)
+{
+    int[] Array = number;
+    int i = 0;
+    int sum = 0;
+
+    while (i < Array)
+    {
+        sum = sum + Array[i];
+    }
+    Console.WriteLine($"Сумма цифр в числе равна: {sum}");
+
+
+}
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+ArraySum(number);
+
+
+// int[] array = {1,2,34,54,23,123,54,23};
+
+// int n = array.Length;
+// int find = 23;
+
+// int index = 0;
+
+// while (index < n)
+// {
+
+//     if(array[index] == find)
+//     {
+//         Console.WriteLine(index);
+//         break;
+//     }
+//     //index = index + 1;
+//     index++;
+//}
