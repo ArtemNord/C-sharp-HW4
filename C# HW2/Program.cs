54978683b87d879b495347f99c2802c4c3bf2ce2
Naganishe <mr.nordakov@mail.ru>
2022-06-20 23:58:05 +0300

    add HW#2 and HW#3

--- C# HW2/Program.cs
+++ C# HW2/Program.cs
@@ -1,41 +1,17 @@
 ﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
-void ArraySum(int number)
+void SummNumber(int number)
 {
-    int[] Array = number;
-    int i = 0;
-    int sum = 0;
-
-    while (i < Array)
+    int result = number % 10;
+    while (number != 0)
     {
-        sum = sum + Array[i];
+        number = number / 10;
+        result = result + (number % 10);
     }
-    Console.WriteLine($"Сумма цифр в числе равна: {sum}");
-
-
+    Console.WriteLine(result);
 }
 
 Console.WriteLine("Введите число:");
 int number = Convert.ToInt32(Console.ReadLine());
 
-ArraySum(number);
-
-
-// int[] array = {1,2,34,54,23,123,54,23};
-
-// int n = array.Length;
-// int find = 23;
-
-// int index = 0;
-
-// while (index < n)
-// {
-
-//     if(array[index] == find)
-//     {
-//         Console.WriteLine(index);
-//         break;
-//     }
-//     //index = index + 1;
-//     index++;
-//}
+SummNumber(number);
---
+++ C# HW3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая задает массв из 8 элементов и выводит их на экран.
+
+int [] Length = new int [8];
+
+void Array(int [] number)
+{
+    for (int i = 0; i < number.Length; i++)
+    {
+        Console.Write("Введите число: ");
+        number[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+Array(Length);
+
+void ArrayShow(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($" {array[i]}");
+    }
+}
+
+ArrayShow(Length);
